30afcb214f2bfaf107c3419937065039725b47e4
leesangha <sanghalee1@naver.com>
2020-10-22 18:39:05 +0900

    app.py

--- app.py
+++ app.py
@@ -15,69 +15,107 @@ from werkzeug.utils import secure_filename
 
 from queue import Empty,Queue
 import threading
+import time
 
 ###################
 requests_queue=Queue()
 BATCH_SIZE = 1
 CHECK_INTERVAL = 0.1
 ###################
-
+status=0
 def makeModel(text,leng,k):
-    model_name='774M'
-    seed=None
-    nsamples=1
-    batch_size=1
-    length=60
-    temperature=1
-    top_k=40
-    top_p=1
-    models_dir='models'
-    raw_text=text
+    try:
+        model_name='774M'
+        seed=None
+        nsamples=1
+        batch_size=1
+        length=60
+        temperature=1
+        top_k=40
+        top_p=1
+        models_dir='models'
+        raw_text=text
 
-    models_dir = os.path.expanduser(os.path.expandvars(models_dir))
-    if batch_size is None:
-        batch_size = 1
-    assert nsamples % batch_size == 0
+        models_dir = os.path.expanduser(os.path.expandvars(models_dir))
+        if batch_size is None:
+            batch_size = 1
+        assert nsamples % batch_size == 0
 
-    enc = encoder.get_encoder(model_name, models_dir)
-    hparams = model.default_hparams()
-    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
-        hparams.override_from_dict(json.load(f))
+        enc = encoder.get_encoder(model_name, models_dir)
+        hparams = model.default_hparams()
+        with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+            hparams.override_from_dict(json.load(f))
 
-    if length is None:
-        length = hparams.n_ctx // 2
-    elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+        if length is None:
+            length = hparams.n_ctx // 2
+        elif length > hparams.n_ctx:
+            raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
-    with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
-        output = sample.sample_sequence(
-            hparams=hparams, length=length,
-            context=context,
-            batch_size=batch_size,
-            temperature=temperature, top_k=top_k, top_p=top_p
-        )
+        with tf.Session(graph=tf.Graph()) as sess:
+            context = tf.placeholder(tf.int32, [batch_size, None])
+            np.random.seed(seed)
+            tf.set_random_seed(seed)
+            output = sample.sample_sequence(
+                hparams=hparams, length=length,
+                context=context,
+                batch_size=batch_size,
+                temperature=temperature, top_k=top_k, top_p=top_p
+            )
 
-        saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
-        saver.restore(sess, ckpt)
-       
-        context_tokens = enc.encode(raw_text)
-        generated = 0
-        for _ in range(nsamples // batch_size):
-            out = sess.run(output, feed_dict={
-                context: [context_tokens for _ in range(batch_size)]
-            })[:, len(context_tokens):]
-            for i in range(batch_size):
-                generated += 1
-                text = enc.decode(out[i])
-                print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                print(text)
-    return text.encode('ascii','ignore').decode('ascii')
+            saver = tf.train.Saver()
+            ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
+            saver.restore(sess, ckpt)
+        
+            context_tokens = enc.encode(raw_text)
+            generated = 0
+            for _ in range(nsamples // batch_size):
+                out = sess.run(output, feed_dict={
+                    context: [context_tokens for _ in range(batch_size)]
+                })[:, len(context_tokens):]
+                for i in range(batch_size):
+                    generated += 1
+                    text = enc.decode(out[i])
+                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+                    print(text)
+        return text.encode('ascii','ignore').decode('ascii')
+    except Exception as e:
+        print(e)
+        return 500
 
+#Queueing
+def handle_requests_by_batch():
+    try:
+        while True:
+            requests_batch=[]
 
+            while not(len(requests_batch) >= BATCH_SIZE):
+                try:
+                    requests_batch.append(
+                        requests_queue.get(timeout=CHECK_INTERVAL)
+                    )
+                except Empty:
+                    continue
+        
+            batch_outputs=[]
+
+            for request in requests_batch:
+                print('run')
+                global status
+                status=1
+                batch_outputs.append(
+                    makeModel(request["input"][0],request["input"][1],request["input"][2])
+                )
+            
+            for request, output in zip(requests_batch,batch_outputs):
+                request["output"]=output
+
+    except Exception as e:
+        while not requests_queue.empty():
+            requests_queue.get()
+        print(e)
+
+#Thread Start
+threading.Thread(target=handle_requests_by_batch).start()
 
 app = Flask(__name__, template_folder="templates", static_url_path="/static")
 
@@ -89,9 +127,32 @@ def main():
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
+        global status
+        if status ==1:
+            return jsonify({"message":"Too Many Requests"}),429
+        print(requests_queue.qsize())
+        if requests_queue.qsize() >=1:
+            return jsonify({"message":"Too Many Requests"}),429
+        
         text=request.form["message"]
-        result=makeModel(text,40,40)
+        length=request.form["length"]
+        top_k=request.form["top_k"]
+        print('receive' + text +' ' + length + ' ' + top_k)
+
+        req={"input":[text,40,40]}
+        requests_queue.put(req)
+        #result=makeModel(text,40,40)
+
+        #Thread output response
+        while "output" not in req:
+            time.sleep(CHECK_INTERVAL)
+
+        if req["output"] == 500:
+            return jsonify({"error": "Error output is something wrong"}), 500
+        status=0
+        result=text+req["output"]
         return jsonify({"message": result}), 200
+
     except Exception as e:
         print(e)
 
